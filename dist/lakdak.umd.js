d89cfc88a519b82f755f5ed33c0c8bdf183cc634
Thach Lockevn <lockevn@gmail.com>
2022-11-08 21:19:16 +0700

    also add dist to repo

---
+++ dist/lakdak.umd.js
@@ -0,0 +1,1 @@
+(function(e,t){typeof exports=="object"&&typeof module<"u"?t(exports):typeof define=="function"&&define.amd?define(["exports"],t):(e=typeof globalThis<"u"?globalThis:e||self,t(e.Lakdak={}))})(this,function(e){"use strict";function t(){return 1}e.MyButton=t,Object.defineProperties(e,{__esModule:{value:!0},[Symbol.toStringTag]:{value:"Module"}})});
