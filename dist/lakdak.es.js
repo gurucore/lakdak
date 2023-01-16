0322952b12b31faea27636024713a428ea99f6c6
Thach Lockevn <lockevn@gmail.com>
2023-01-16 14:28:32 +0700

    v0.0.2 build

--- dist/lakdak.es.js
+++ dist/lakdak.es.js
@@ -1,6 +1,16 @@
-function t() {
+function a() {
   return 1;
 }
+const s = {};
+class d {
+  static getCurrentServerIpAddresses(r = "v4") {
+    return Object.values(s.networkInterfaces()).reduce((e, t) => (e = e.concat(t), e), []).filter(({ family: e, address: t }) => e.toLowerCase().indexOf(r) >= 0 && t !== "127.0.0.1").map(({ address: e }) => e).join(", ");
+  }
+  static extractIpFromRequest(r) {
+    return r?.socket?.remoteAddress || r?.headers["x-forwarded-for"];
+  }
+}
 export {
-  t as MyButton
+  d as DevOpsHelper,
+  a as testFunction
 };
