5451ebc63be84e9d2d4b9118e96455981a8706ed
Thach Lockevn <lockevn@gmail.com>
2025-02-09 10:53:46 +0700

    setup: update to use newest eslint

---
+++ eslint.config.js
@@ -0,0 +1,13 @@
+// use type=module in package.json if we want to use ES6 modules `export default {}`
+module.exports = {
+  ignores: [
+    'node_modules',
+    '.parcel-cache',
+    'dist',
+    '.eslintrc.js',
+    'package.json',
+    'package-lock.json',
+    'coverage'
+    //
+  ]
+}
