dc06c62d79b723007b561ea90ed023ecdaad0db9
Thach Lockevn <lockevn@gmail.com>
2025-03-04 23:29:34 +0700

    chore: comment

--- src/models/CustomError.ts
+++ src/models/CustomError.ts
@@ -1,7 +1,12 @@
+/** Custom Error class, with support extra information about the exception */
 export class CustomError extends Error {
   constructor(
     message: string,
+
+    /** original error */
     public readonly error?: Error | unknown,
+
+    /** extra information about the exception, attached to this Error */
     public readonly extra?: any
   ) {
     super(message)
